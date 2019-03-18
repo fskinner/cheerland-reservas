e4167a3218f90c8e280101d70c85ac2a53c3f430
Felipe Skinner <felipeskinner@gmail.com>
2019-03-18 10:05:15 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,20 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :cheerland_reservas, CheerlandReservasWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: System.get_env("PORT")],
+  url: [scheme: "https", host: "whispering-cliffs-80647.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :cheerland_reservas, CheerlandReservas.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
