c5bd70fc50ffa34f8bb560fe734e04337d12c494
Felipe Skinner <felipeskinner@gmail.com>
2019-10-27 08:28:28 -0300

    Fix seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,19 +5,21 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-# CheerlandReservas.Repo.insert!(%CheerlandReservas.Authentication.User{
-#   email: "lucasavilez@gmail.com",
-#   name: "Lucas Avilez",
-#   password: "a12345A",
-#   gender: "Masculino"
-# })
+CheerlandReservas.Authentication.create_user(%{
+  email: "lucasavilez@gmail.com",
+  name: "Lucas Avilez",
+  password: "a12345A",
+  gender: "Masculino"
+  is_admin: true
+})
 
-# CheerlandReservas.Repo.insert!(%CheerlandReservas.Authentication.User{
-#   email: "felipeskinner@gmail.com",
-#   name: "Felipe Skinner",
-#   password: "a12345A",
-#   gender: "Masculino"
-# })
+CheerlandReservas.Authentication.create_user(%{
+  email: "felipeskinner@gmail.com",
+  name: "Felipe Skinner",
+  password: "a12345A",
+  gender: "Masculino",
+  is_admin: true
+})
 
 #
 # We recommend using the bang functions (`insert!`, `update!`
