6c9653bfb5c24f1ab06203765871470c39d50845
Felipe Skinner <felipeskinner@gmail.com>
2019-10-30 09:36:00 -0300

    Uncomment failling tests

--- test/cheerland_reservas_web/controllers/room_controller_test.exs
+++ test/cheerland_reservas_web/controllers/room_controller_test.exs
@@ -12,75 +12,75 @@ defmodule CheerlandReservasWeb.RoomControllerTest do
     room
   end
 
-  # describe "index" do
-  #   test "lists all rooms", %{conn: conn} do
-  #     conn = get(conn, Routes.room_path(conn, :index))
-  #     assert html_response(conn, 200) =~ "Lista dos Quartos"
-  #   end
-  # end
+  describe "index" do
+    test "lists all rooms", %{conn: conn} do
+      conn = get(conn, Routes.room_path(conn, :index))
+      assert html_response(conn, 200) =~ "Lista dos Quartos"
+    end
+  end
 
-  # describe "new room" do
-  #   test "renders form", %{conn: conn} do
-  #     conn = get(conn, Routes.room_path(conn, :new))
-  #     assert html_response(conn, 200) =~ "Novo Quarto"
-  #   end
-  # end
+  describe "new room" do
+    test "renders form", %{conn: conn} do
+      conn = get(conn, Routes.room_path(conn, :new))
+      assert html_response(conn, 200) =~ "Novo Quarto"
+    end
+  end
 
-  # describe "create room" do
-  #   test "redirects to show when data is valid", %{conn: conn} do
-  #     conn = post(conn, Routes.room_path(conn, :create), room: @create_attrs)
+  describe "create room" do
+    test "redirects to show when data is valid", %{conn: conn} do
+      conn = post(conn, Routes.room_path(conn, :create), room: @create_attrs)
 
-  #     assert %{id: id} = redirected_params(conn)
-  #     assert redirected_to(conn) == Routes.room_path(conn, :show, id)
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == Routes.room_path(conn, :show, id)
 
-  #     conn = get(conn, Routes.room_path(conn, :show, id))
-  #     assert html_response(conn, 200) =~ @create_attrs.label
-  #   end
+      conn = get(conn, Routes.room_path(conn, :show, id))
+      assert html_response(conn, 200) =~ @create_attrs.label
+    end
 
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post(conn, Routes.room_path(conn, :create), room: @invalid_attrs)
-  #     assert html_response(conn, 200) =~ "Novo Quarto"
-  #   end
-  # end
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.room_path(conn, :create), room: @invalid_attrs)
+      assert html_response(conn, 200) =~ "Novo Quarto"
+    end
+  end
 
-  # describe "edit room" do
-  #   setup [:create_room]
+  describe "edit room" do
+    setup [:create_room]
 
-  #   test "renders form for editing chosen room", %{conn: conn, room: room} do
-  #     conn = conn |> Plug.Test.init_test_session(%{}) |> get(Routes.room_path(conn, :edit, room))
-  #     assert html_response(conn, 200) =~ "Editar Quarto"
-  #   end
-  # end
+    test "renders form for editing chosen room", %{conn: conn, room: room} do
+      conn = conn |> Plug.Test.init_test_session(%{}) |> get(Routes.room_path(conn, :edit, room))
+      assert html_response(conn, 200) =~ "Editar Quarto"
+    end
+  end
 
-  # describe "update room" do
-  #   setup [:create_room]
+  describe "update room" do
+    setup [:create_room]
 
-  #   test "redirects when data is valid", %{conn: conn, room: room} do
-  #     conn = put(conn, Routes.room_path(conn, :update, room), room: @update_attrs)
-  #     assert redirected_to(conn) == Routes.room_path(conn, :show, room)
+    test "redirects when data is valid", %{conn: conn, room: room} do
+      conn = put(conn, Routes.room_path(conn, :update, room), room: @update_attrs)
+      assert redirected_to(conn) == Routes.room_path(conn, :show, room)
 
-  #     conn = get(conn, Routes.room_path(conn, :show, room))
-  #     assert html_response(conn, 200) =~ "some updated label"
-  #   end
+      conn = get(conn, Routes.room_path(conn, :show, room))
+      assert html_response(conn, 200) =~ "some updated label"
+    end
 
-  #   test "renders errors when data is invalid", %{conn: conn, room: room} do
-  #     conn = put(conn, Routes.room_path(conn, :update, room), room: @invalid_attrs)
-  #     assert html_response(conn, 200) =~ "Editar Quarto"
-  #   end
-  # end
+    test "renders errors when data is invalid", %{conn: conn, room: room} do
+      conn = put(conn, Routes.room_path(conn, :update, room), room: @invalid_attrs)
+      assert html_response(conn, 200) =~ "Editar Quarto"
+    end
+  end
 
-  # describe "delete room" do
-  #   setup [:create_room]
+  describe "delete room" do
+    setup [:create_room]
 
-  #   test "deletes chosen room", %{conn: conn, room: room} do
-  #     conn = delete(conn, Routes.room_path(conn, :delete, room))
-  #     assert redirected_to(conn) == Routes.room_path(conn, :index)
+    test "deletes chosen room", %{conn: conn, room: room} do
+      conn = delete(conn, Routes.room_path(conn, :delete, room))
+      assert redirected_to(conn) == Routes.room_path(conn, :index)
 
-  #     assert_error_sent(404, fn ->
-  #       get(conn, Routes.room_path(conn, :show, room))
-  #     end)
-  #   end
-  # end
+      assert_error_sent(404, fn ->
+        get(conn, Routes.room_path(conn, :show, room))
+      end)
+    end
+  end
 
   defp create_room(_) do
     room = fixture(:room)
